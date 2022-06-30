7b2ffa4767738e6158b817818cb6206dfe15ac6f
jemini-bhut <18ce014.jemini.bhut@vvpedulink.ac.in>
2022-06-30 18:37:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let count = 0;
+let saveE1 = document.getElementById("save-el");
+let countE1 = document.getElementById("count-el");
+
+function increment() {
+    count += 1;
+    countE1.textContent = count;
+}
+
+function save() {
+    let countStr = count + " - ";
+    saveE1.textContentStr;
+    document.getElementById("save-el").innerHTML = count;
+    countE1.textContent = 0;
+    count = 0;
+}
+
+
+
+console.log("Let's count people on the station.");
+
+

885e05765aaa4637883b84aa1fc0237b7a816a9d
Jemini Bhut <58567102+jemini-bhut@users.noreply.github.com>
2022-07-01 09:10:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ let count = 0;
 let saveE1 = document.getElementById("save-el");
 let countE1 = document.getElementById("count-el");
 
+// Function to count number of people entering 
 function increment() {
     count += 1;
     countE1.textContent = count;
 }
 
+// Function to show and display last number of entries
 function save() {
     let countStr = count + " - ";
     saveE1.textContentStr;
@@ -15,8 +17,4 @@ function save() {
     count = 0;
 }
 
-
-
 console.log("Let's count people on the station.");
-
-
